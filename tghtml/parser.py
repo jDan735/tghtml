37b6fc09731af096db3288847d9d4cadbf5a1ca6
jDan735 <daniel734@bk.ru>
2021-01-22 23:36:11 +0300

    Support more paragraphs

--- tghtml/parser.py
+++ tghtml/parser.py
@@ -8,6 +8,33 @@ def unTag(tag_name, tag):
               .replace(f"</{tag_name}>", "")
 
 
+def clearTag(soup):
+    for tag in soup():
+        for attribute in [*tag.attrs]:
+            try:
+                del tag[attribute]
+            except Exception:
+                pass
+
+    allowedTags = ["b", "strong", "i", "em", "code", "s",
+                   "strike", "del", "u", "pre"]
+
+    page = str(soup)
+
+    page = re.sub(r"\[.{0,}?\]", "", page)
+
+    page = page.replace("<img/>", "") \
+               .replace("<br/>", "") \
+               .replace("<br>", "")
+    page = unTag("p", page)
+
+    for tag in soup():
+        if not (tag.name in allowedTags):
+            page = unTag(tag.name, page)
+
+    return page
+
+
 def tghtml(page, tagBlocklist=[]):
     soup = BeautifulSoup(page, 'lxml')
 
@@ -30,30 +57,13 @@ def tghtml(page, tagBlocklist=[]):
         pass
 
     try:
-        soup = soup.p
+        p = ""
 
-        for tag in soup():
-            for attribute in [*tag.attrs]:
-                try:
-                    del tag[attribute]
-                except Exception:
-                    pass
+        for tag in soup.findAll("p"):
+            p += unTag("p", str(clearTag(tag))) + "\n"
 
-        allowedTags = ["b", "strong", "i", "em", "code", "s",
-                       "strike", "del", "u", "pre"]
-
-        page = str(soup)
-
-        page = re.sub(r"\[.{0,}?\]", "", page)
-
-        page = page.replace("<img/>", "") \
-                   .replace("<br/>", "") \
-                   .replace("<br>", "")
-        page = unTag("p", page)
-
-        for tag in soup():
-            if not (tag.name in allowedTags):
-                page = unTag(tag.name, page)
+        soup = BeautifulSoup(p)
+        page = str(clearTag(soup))
 
         return page
 
